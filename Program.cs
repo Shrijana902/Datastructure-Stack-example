2dead38b7da5f1a98a099364cf01e2ba79b16ca0
Shrijana Basnet <basnetshrijana902@gmail.com>
2022-02-04 14:56:31 +0545

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace StackExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack my_stack=new Stack();
+            my_stack.Push("sita");
+            my_stack.Push("Rita");
+            my_stack.Push("Ram");
+            my_stack.Push("Shayam");
+            foreach(string str in my_stack)
+            {
+                Console.WriteLine(str);
+
+            }
+            Console.WriteLine("After poping the elements");
+            my_stack.Pop();
+            foreach(string str in my_stack)
+            {
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
